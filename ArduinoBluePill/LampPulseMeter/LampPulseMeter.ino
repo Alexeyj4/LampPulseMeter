6ecba74c536d71019b254d4331c6da4356202f24
Vlad <Klisunov.vladdi94@gmail.com>
2023-11-07 16:23:02 +0300

    Update LampPulseMeter.ino

--- ArduinoBluePill/LampPulseMeter/LampPulseMeter.ino
+++ ArduinoBluePill/LampPulseMeter/LampPulseMeter.ino
@@ -1,9 +1,20 @@
+<<<<<<< Updated upstream
 const int photo_r_input_pin=PA0; //A0=PA0
 const int avg_filter_value=20000; //filter for calc min and max
 const float freq_coef=1.7857; //coef for calc freq
 
 int adc_thr_min;//threshold for freq meas
 int adc_thr_max;//threshold for freq meas
+=======
+//схема устройства простая, поэтому в KiCade не оформлял:
+//плата BluePill подключена к OLED-дисплею через I2C
+//фоторизистор GL5528 один конец к +3,3 второй конец - к A0,A1. Подтягивающие резисторы испрользуются встроенные в BluePill
+//АЦП считывается с одного из них
+
+const int photo_r_input_pin0=PA0; //A0=PA0
+const int photo_r_input_pin1=PA1; //A1=PA1
+const int avg_filter_value=10000; //filter for calc min and max
+>>>>>>> Stashed changes
 
 #include <Oled.h>
 Oled oled;
@@ -11,7 +22,8 @@ Oled oled;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(photo_r_input_pin, INPUT_PULLDOWN);
+  pinMode(photo_r_input_pin0, INPUT_PULLDOWN);
+  pinMode(photo_r_input_pin1, INPUT_PULLDOWN);
   oled.begin();
 }
 
@@ -23,7 +35,7 @@ void loop() {
   bool sine_upper_level;
   
   for(int i;i<avg_filter_value;i++){
-    int adc=analogRead(photo_r_input_pin);
+    int adc=analogRead(photo_r_input_pin0);
     if(adc<adc_min)adc_min=adc;
     if(adc>adc_max)adc_max=adc;
     if((adc<adc_thr_min)and(sine_upper_level)){
